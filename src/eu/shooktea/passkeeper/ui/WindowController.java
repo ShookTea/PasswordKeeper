d8de8d52f7b16a9a8c1c75f922ebbf01762f2f97
Norbert Kowalik <kowaliknor@gmail.com>
2018-12-06 18:56:16 +0100

    Set up main window GUI to support passwords

--- src/eu/shooktea/passkeeper/ui/WindowController.java
+++ src/eu/shooktea/passkeeper/ui/WindowController.java
@@ -2,21 +2,28 @@ package eu.shooktea.passkeeper.ui;
 
 import eu.shooktea.passkeeper.Type;
 import eu.shooktea.passkeeper.type.Note;
+import eu.shooktea.passkeeper.type.Password;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
 public class WindowController {
 
-    @FXML
-    private TableView<Note> notesTable;
+    @FXML private TableView<Note> notesTable;
+    @FXML private TableView<Password> passwordsTable;
 
     @FXML
     private void initialize() {
         Type.NOTE.applyTableViewMaker(notesTable);
+        Type.PASSWORD.applyTableViewMaker(passwordsTable);
     }
 
     @FXML
     private void createNewNote() {
         Type.NOTE.showNewWindow();
+    }
+
+    @FXML
+    private void createNewPassword() {
+        Type.PASSWORD.showNewWindow();
     }
 }
