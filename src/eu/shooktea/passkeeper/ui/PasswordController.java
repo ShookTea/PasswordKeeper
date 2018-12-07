b9542aa53dc5e80cae8887b9fd59fc1bb761cb62
Norbert Kowalik <kowaliknor@gmail.com>
2018-12-07 19:58:07 +0100

    Created GUI and controller for password edition

---
+++ src/eu/shooktea/passkeeper/ui/PasswordController.java
@@ -0,0 +1,26 @@
+package eu.shooktea.passkeeper.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PasswordController {
+
+    @FXML private TextField passwordName;
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private TextField url;
+
+    @FXML
+    private void savePassword() {
+
+    }
+
+    @FXML
+    private void goBack() {
+
+    }
+
+    @FXML
+    private void deletePassword() {
+    }
+}
